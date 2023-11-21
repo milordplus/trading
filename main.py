5d51dcd4e7cd821c29d3df51fcb41295987fcf80
milordplus <milordplus@mail.ru>
2023-11-21 23:47:39 +0300

    02

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(
-    title="Trading"
+    title="Trading App"
 )
 
 fake_users = [
@@ -14,6 +17,15 @@ fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
     {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
 ]
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str
+    side: str
+    price: float
+    amount: float
 
 
 @app.get("/users")
@@ -36,3 +48,9 @@ def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
